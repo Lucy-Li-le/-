95ac5b20c197886d9d760929812a577ba606b448
Lucy-Li-le <Lucy-Li-le@users.noreply.github.com>
2026-09-05 14:22:32 +0800

    Configure new GrowthProof contract

--- contract-config.js
+++ contract-config.js
@@ -1,7 +1,6 @@
 window.GROWTH_PROOF_CONFIG = {
   // Paste the complete address of the newly deployed GrowthProof contract here.
-  address: "",
+  address: "0x58BD27398C251Dea02Be46b5752D3502D62AEe7A",
   chainId: 10143,
   explorerUrl: "https://testnet.monadscan.com",
 };
-
